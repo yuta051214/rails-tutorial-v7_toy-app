596b44420543cf37ac9acfd6eca06bc7bf432ac3
yuta051214 <ffftrick123@gmail.com>
2023-06-18 22:40:18 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :microposts
 
-  validates :name, presence: ture
-  validates :email, presence: ture
+  validates :name, presence: true
+  validates :email, presence: true
 end
