76c46e7632532ab1afe831be70bb7425021c7036
yuta051214 <ffftrick123@gmail.com>
2023-06-18 20:04:54 +0900

    Add microposts to users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    user = User.find(params[:id])
+    @microposts = Micropost.where(user_id: user.id)
   end
 
   # GET /users/new
